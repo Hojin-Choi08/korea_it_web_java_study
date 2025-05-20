7bb59cd9e94af5b0c93d8523dea70870d7b09d17
Hojin Choi <merro3043@gmail.com>
2025-05-20 15:42:02 +0900

    MultipleTable

--- src/_12_MultipleTable/MultipleTable.java
+++ src/_12_MultipleTable/MultipleTable.java
@@ -6,11 +6,10 @@ public class MultipleTable {
         //1 X 3 = 3
         for (int i = 1; i < 10; i++) {
             for (int j = 1; j < 10; j++) {
-                System.out.println(i + "X" + j + "=" + i*j);
+                System.out.println(i + "X" + j + "=" + i * j);
             }
             System.out.println();
         }
-        int hi;
     }
 }
 
