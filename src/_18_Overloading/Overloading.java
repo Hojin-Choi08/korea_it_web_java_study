06a63e08904723ac4dd65040909bda7ad09f9970
Hojin Choi <merro3043@gmail.com>
2025-05-21 21:49:06 +0900

    Overloading

--- src/_18_Overloading/Overloading.java
+++ src/_18_Overloading/Overloading.java
@@ -34,6 +34,23 @@ public class Overloading {
     public static void printInfo(String name, int age, String mail) {
         System.out.println(name + " 그리고 " + age + " 메일은 ?: " + mail);
     }
+
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int add(double doubleNum1, double doubleNum2) {
+        int num1 = (int) doubleNum1;
+        int num2 = (int) doubleNum2;
+        return num1 + num2;
+
+    }
+
+    public static int add(String strNum1, String strNum2) {
+        int num1 = Integer.parseInt(strNum1);
+        int num2 = Integer.parseInt(strNum2);
+        return num1 + num2;
+    }
     public static void main(String[] args) {
         //메소드 오버로딩
         //같은 이름의 메소드를 여러 개 선언
@@ -52,6 +69,14 @@ public class Overloading {
         printInfo("Asher");
         printInfo("Hunter", 18);
         printInfo("Owen", 19, "메일");
+
+        //메소드 이름 add()
+        //int와 int 덧셈
+        //double과 double 덧셈
+        //문자열숫자와 문자열 숫자 덧셈
+        System.out.println(add(1, 2));
+        System.out.println(add("3", "4"));
+        System.out.println(add(3.2, 4.5));
 
     }
 }
