d044746ee31ff96df9df958fd942763ef59d9445
Hojin Choi <merro3043@gmail.com>
2025-05-30 21:44:12 +0900

    Singleton

---
+++ src/main/java/_30_Singleton/AppConfig/AppConfig.java
@@ -0,0 +1,25 @@
+package _30_Singleton.AppConfig;
+
+import lombok.Data;
+//Getter, Setter, toString, EqualsAndHashCode and RequiredArgsConstructor
+@Data //자주 쓰는 시그니처 패키지
+public class AppConfig {
+    private static AppConfig instance;
+
+    private String apikey;
+    private String appMode;
+
+    private AppConfig() {
+        this.apikey = "DEFAULT - KEY";
+        this.appMode = "PRODUCTION";
+    }
+
+    public static AppConfig getInstance() {
+        if (instance == null) {
+            instance = new AppConfig();
+        }
+        return instance;
+    }
+
+
+}
